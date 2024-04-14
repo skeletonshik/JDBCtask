fafc0094d0675bc044b9efb76317676a07d7b7c2
skeletonshik <warhameros@gmail.com>
2024-04-15 00:59:07 +0500

    clear

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -13,23 +13,6 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) throws SQLException {
-//        UserService service = new UserServiceImpl();
-//
-//        service.createUsersTable();
-//
-//        service.saveUser("Name1", "LastName1", (byte) 20);
-//        service.saveUser("Name2", "LastName2", (byte) 25);
-//        service.saveUser("Name3", "LastName3", (byte) 31);
-//        service.saveUser("Name4", "LastName4", (byte) 38);
-//
-//        service.removeUserById(1);
-//        service.getAllUsers();
-//        service.cleanUsersTable();
-//        service.dropUsersTable();
-//
-//        service.closeConnection();
-
-
         UserService service = new UserServiceImpl();
 
         service.createUsersTable();
